adcd9bb9c49b3223944ca159757c155012178a6b
Florian <dekontaminator@googlemail.com>
2016-08-25 21:47:33 +0200

    use normal swing to render html

--- src/main/java/de/florianmarsch/picture/Screenshot.java
+++ src/main/java/de/florianmarsch/picture/Screenshot.java
@@ -1,32 +1,60 @@
 package de.florianmarsch.picture;
 
-import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-
-import gui.ava.html.image.generator.HtmlImageGenerator;
+import javax.swing.JEditorPane;
 
 public class Screenshot {
+	
+	private Boolean wait = Boolean.TRUE;
 
 	public File save(String url) {
 		int width = 458, height = 228;
 		// Create a `BufferedImage` and create the its `Graphics`
-	
-		
-		HtmlImageGenerator gen = new HtmlImageGenerator();
-		gen.setSize(new Dimension(width, height));
-		gen.loadUrl(url);
-		BufferedImage image = gen.getBufferedImage();
-		
-		// Output the `BufferedImage` via `ImageIO`
+		BufferedImage image = null;
 
 		File output = null;
 		try {
+
 			output = File.createTempFile("temp", ".png");
-			System.out.println("uses file "+output.getAbsolutePath());
+			image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+			JEditorPane panel = new JEditorPane(url);
+			panel.setSize(width, height);
+			panel.addPropertyChangeListener("page",new PropertyChangeListener() {
+				
+				@Override
+				public void propertyChange( PropertyChangeEvent evt) {
+					System.out.println(evt.getPropertyName());
+					wait = Boolean.FALSE;
+					
+				}
+			});
+			Integer timer = 0;
+			while (wait) {
+				try {
+					Thread.sleep(1);
+					timer = timer +1 ;
+					if(timer > 4000){
+						wait = Boolean.FALSE;
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			wait = Boolean.TRUE;
+			panel.paint(image.createGraphics());
+			
+			
+			
+			System.out.println("uses file " + output.getAbsolutePath());
 			ImageIO.write(image, "png", output);
 			System.out.println("image on file");
 		} catch (IOException e) {
@@ -34,7 +62,5 @@ public class Screenshot {
 		}
 		return output;
 	}
-
-	
 
 }
