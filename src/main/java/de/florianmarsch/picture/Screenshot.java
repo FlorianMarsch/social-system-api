8e2e5e1d56614ac0271b77e4b3ceb61e65b1f1a9
Florian <dekontaminator@googlemail.com>
2016-08-25 20:01:46 +0200

    try html2image lib

--- src/main/java/de/florianmarsch/picture/Screenshot.java
+++ src/main/java/de/florianmarsch/picture/Screenshot.java
@@ -1,34 +1,26 @@
 package de.florianmarsch.picture;
 
-import java.awt.Graphics;
-import java.awt.GraphicsEnvironment;
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.UUID;
 
 import javax.imageio.ImageIO;
-import javax.swing.JEditorPane;
+
+import gui.ava.html.image.generator.HtmlImageGenerator;
 
 public class Screenshot {
 
 	public File save(String url) {
-		String html = loadFile(url);
 		int width = 458, height = 228;
 		// Create a `BufferedImage` and create the its `Graphics`
-		BufferedImage image = new BufferedImage(width, height,
-                BufferedImage.TYPE_INT_ARGB);
-		Graphics graphics = image.createGraphics();
-		// Create an `JEditorPane` and invoke `print(Graphics)`
-		JEditorPane jep = new JEditorPane("text/html", html);
-		jep.setSize(width, height);
-		jep.print(graphics);
+	
+		
+		HtmlImageGenerator gen = new HtmlImageGenerator();
+		gen.setSize(new Dimension(width, height));
+		gen.loadUrl(url);
+		BufferedImage image = gen.getBufferedImage();
+		
 		// Output the `BufferedImage` via `ImageIO`
 
 		File output = null;
@@ -43,27 +35,6 @@ public class Screenshot {
 		return output;
 	}
 
-	private String loadFile(String url) {
-
-		StringBuffer tempReturn = new StringBuffer();
-		try {
-			URL u = new URL(url);
-			InputStream is = u.openStream();
-			DataInputStream dis = new DataInputStream(new BufferedInputStream(is));
-			String s;
-
-			while ((s = dis.readLine()) != null) {
-				tempReturn.append(s);
-			}
-
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return tempReturn.toString();
-	}
+	
 
 }
