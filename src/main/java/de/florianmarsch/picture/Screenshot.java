846fdad49906b121cf6de4e9a2535672ed6df6ed
Florian <dekontaminator@googlemail.com>
2016-08-26 18:16:21 +0200

    use google analytics to get picture

--- src/main/java/de/florianmarsch/picture/Screenshot.java
+++ src/main/java/de/florianmarsch/picture/Screenshot.java
@@ -3,64 +3,85 @@ package de.florianmarsch.picture;
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
 
 import javax.imageio.ImageIO;
 import javax.swing.JEditorPane;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Screenshot {
-	
+
 	private Boolean wait = Boolean.TRUE;
 
 	public File save(String url) {
-		int width = 458, height = 228;
-		// Create a `BufferedImage` and create the its `Graphics`
-		BufferedImage image = null;
+		String screensurl = "https://www.googleapis.com/pagespeedonline/v1/runPagespeed?url=" + url
+				+ "&screenshot=true&strategy=mobile";
+		String content = loadFile(screensurl);
+		String base64image = null;
 
 		File output = null;
 		try {
+			base64image = new JSONObject(content).getJSONObject("screenshot").getString("data").replace("_", "/")
+					.replace("-", "+");
+			System.out.println(base64image);
+			byte[] decoded = Base64.getDecoder().decode(base64image);
+
 
 			output = File.createTempFile("temp", ".png");
-			image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
-			JEditorPane panel = new JEditorPane(url);
-			panel.setSize(width, height);
-			panel.addPropertyChangeListener("page",new PropertyChangeListener() {
-				
-				@Override
-				public void propertyChange( PropertyChangeEvent evt) {
-					System.out.println(evt.getPropertyName());
-					wait = Boolean.FALSE;
-					
-				}
-			});
-			Integer timer = 0;
-			while (wait) {
-				try {
-					Thread.sleep(1);
-					timer = timer +1 ;
-					if(timer > 4000){
-						wait = Boolean.FALSE;
-					}
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-			wait = Boolean.TRUE;
-			panel.paint(image.createGraphics());
-			
-			
-			
 			System.out.println("uses file " + output.getAbsolutePath());
-			ImageIO.write(image, "png", output);
+			
+			FileUtils.writeByteArrayToFile(output, decoded);
+			
+			
+			BufferedImage originalImgage = ImageIO.read(output);
+
+	        BufferedImage subImgage = originalImgage.getSubimage(0, 0, 320, 160);
+			
+	        ImageIO.write(subImgage, "png", output);
+	        
 			System.out.println("image on file");
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			throw new RuntimeException(e1.getMessage(), e1);
+		}
+
+		return output;
+	}
+
+	private String loadFile(String url) {
+
+		StringBuffer tempReturn = new StringBuffer();
+		try {
+			URL u = new URL(url);
+			InputStream is = u.openStream();
+			DataInputStream dis = new DataInputStream(new BufferedInputStream(is));
+			String s;
+
+			while ((s = dis.readLine()) != null) {
+				tempReturn.append(s);
+			}
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return output;
+		return tempReturn.toString();
 	}
 
 }
