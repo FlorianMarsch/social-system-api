a381263cc7a9718ee0afc28e4b1e22815e1e614f
florianmarsch <florianmarsch@192.168.0.143>
2018-03-09 19:27:08 +0100

    add missed slack vo

---
+++ src/main/java/de/florianmarsch/vo/SlackVO.java
@@ -0,0 +1,43 @@
+package de.florianmarsch.vo;
+
+public class SlackVO {
+	private String channel;
+	private String accessToken;
+	private String tweet;
+	private String image;
+	private Boolean isImage;
+	public String getChannel() {
+		return channel;
+	}
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+	public String getAccessToken() {
+		return accessToken;
+	}
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+	public String getTweet() {
+		return tweet;
+	}
+	public void setTweet(String tweet) {
+		this.tweet = tweet;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public Boolean getIsImage() {
+		return isImage;
+	}
+	public void setIsImage(Boolean isImage) {
+		this.isImage = isImage;
+	}
+	
+	
+	
+	
+}
