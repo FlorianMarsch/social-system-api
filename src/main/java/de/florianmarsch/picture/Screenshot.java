9b599167466256a96bf36dceb34e5f0994f018ff
florianmarsch <florianmarsch@192.168.0.144>
2018-11-11 20:47:16 +0100

    https://havecamerawilltravel.com/photographer/instagram-square/

--- src/main/java/de/florianmarsch/picture/Screenshot.java
+++ src/main/java/de/florianmarsch/picture/Screenshot.java
@@ -62,7 +62,7 @@ public class Screenshot {
 			
 			BufferedImage originalImgage = ImageIO.read(output);
 
-	        BufferedImage subImgage = originalImgage;//originalImgage.getSubimage(0, 0, 320, 160);
+	        BufferedImage subImgage = originalImgage.getSubimage(0, 0, 320, (int)((160/2)*1.91));
 			
 	        ImageIO.write(subImgage, "png", output);
 	        
