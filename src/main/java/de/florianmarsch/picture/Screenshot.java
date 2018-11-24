11b654c21bbb4fc5c655c866cebea7db82148a13
florianmarsch <florianmarsch@10.68.55.212>
2018-11-24 11:48:28 +0100

    use story format (9:16)

--- src/main/java/de/florianmarsch/picture/Screenshot.java
+++ src/main/java/de/florianmarsch/picture/Screenshot.java
@@ -62,7 +62,7 @@ public class Screenshot {
 			
 			BufferedImage originalImgage = ImageIO.read(output);
 
-	        BufferedImage subImgage = originalImgage.getSubimage(0, 0, 320, 168);
+	        BufferedImage subImgage = originalImgage.getSubimage(0, 0, 320, 569);
 			
 	        ImageIO.write(subImgage, "png", output);
 	        
